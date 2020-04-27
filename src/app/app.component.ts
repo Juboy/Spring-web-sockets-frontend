47570802166beb1fd34b92121cd2b560c550cb26
Juboy <aderibolejuwon@yahoo.com>
2020-04-27 13:34:18 +0300

    Changed socket endpoint to a local machine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,38 @@
-import { Component } from '@angular/core';
-import Stomp from 'stompjs';
-import SockJS from 'sockjs-client';
-import $ from 'jquery'
+import { Component } from "@angular/core";
+import Stomp from "stompjs";
+import SockJS from "sockjs-client";
+import $ from "jquery";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  private serverUrl = 'http://192.168.1.25:8888/socket'
+  private serverUrl = "http://localhost:8888/socket";
   private stompClient;
-  title = 'websoc';
+  title = "websoc";
 
-  constructor(){
+  constructor() {
     this.initializeWebSocketConnection();
   }
 
-  initializeWebSocketConnection(){
+  initializeWebSocketConnection() {
     let ws = new SockJS(this.serverUrl);
     this.stompClient = Stomp.over(ws);
     let that = this;
-    this.stompClient.connect({}, function(frame) {
+    this.stompClient.connect({}, function (frame) {
       that.stompClient.subscribe("/chat", (message) => {
-        if(message.body) {
-          $(".chat").append("<div class='message'>"+message.body+"</div>")
+        if (message.body) {
+          $(".chat").append("<div class='message'>" + message.body + "</div>");
           console.log(message);
         }
       });
     });
   }
 
-  sendMessage(message){
-    this.stompClient.send("/app/send/message" , {}, message);
-    $('#input').val('');
+  sendMessage(message) {
+    this.stompClient.send("/app/send/message", {}, message);
+    $("#input").val("");
   }
 }
